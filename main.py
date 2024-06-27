a1ddb4f29b97afa77c20ced3c10a1855c4fd07eb
IvanBalyasnikov <balalaika22813337@gmail.com>
2024-06-27 15:17:35 +0400

    try to fix linux moment

--- main.py
+++ main.py
@@ -351,8 +351,8 @@ async def set_task(request: Request):
     target_file = body["file_id"] + found_file
 
     subprocess_data = {
-        "-i" : files_dir_path + api_token_ + "/" + target_file,
-        "-o" : output_files_path + api_token_ + "/" + target_file,
+        "-i" : os.path.abspath(files_dir_path + api_token_ + "/" + target_file),
+        "-o" : os.path.abspath(output_files_path + api_token_ + "/" + target_file),
         "-s" : "2" if "scale" not in body.keys() else body["scale"] if int(body["scale"]) <= 4 else "2",
         "-n" : "realesrgan-x4plus" if "model" not in body.keys() else body["model"] if body["model"] in models_list else "realesrgan-x4plus",
     }
