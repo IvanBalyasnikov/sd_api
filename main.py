5f66e2410ef907d7fb08c21bec42e75638f0e493
IvanBalyasnikov <balalaika22813337@gmail.com>
2024-07-01 20:12:43 +0400

    new api logic

--- main.py
+++ main.py
@@ -41,11 +41,20 @@ def find_file(file_name, directory):
 
 
 def run_cmd(cmd: str):
-    subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    # subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    subprocess.Popen(cmd)
+
+
+def run_task(func, *args):
+    print(func)
+    image = func(*args[:-2])
+    cv2.imwrite(args[-2], ip.b64_cv_im(image))
+    args[-1].add_task(remove_file_delay, args[-2], delay = del_delay)
+
 
 
 @app.post("/txt2img")
-async def txt2img(request: Request):
+async def txt2img(request: Request, background_tasks: BackgroundTasks):
     headers = request.headers
 
     api_token_ = headers.get("api-token")
@@ -73,12 +82,20 @@ async def txt2img(request: Request):
     else:
         h = 512
 
-    image = SDApi.text2img(prompt, h, w)
-    return Response(json.dumps({"image": image}), 200)
+
+    os.makedirs(output_files_path + api_token_ + "/", exist_ok=True)
+    
+    unique_id = str(uuid.uuid4())
+    file_path =  output_files_path + api_token_ + "/" + unique_id + ".png"
+
+    background_tasks.add_task(run_task, SDApi.text2img, prompt, h, w, file_path, background_tasks)
+
+
+    return Response(json.dumps({"job_id": unique_id}), 200)
 
 
 @app.post("/depth2img")
-async def depth2img(request: Request):
+async def depth2img(request: Request, background_tasks: BackgroundTasks):
     headers = request.headers
 
     api_token_ = headers.get("api-token")
@@ -111,12 +128,19 @@ async def depth2img(request: Request):
     else:
         h = 512
 
-    image = SDApi.depth2img(image, prompt, h, w)
-    return Response(json.dumps({"image": image}), 200)
+    os.makedirs(output_files_path + api_token_ + "/", exist_ok=True)
+    
+    unique_id = str(uuid.uuid4())
+    file_path =  output_files_path + api_token_ + "/" + unique_id + ".png"
+
+    background_tasks.add_task(run_task, SDApi.depth2img, image, prompt, h, w, file_path, background_tasks)
+
+
+    return Response(json.dumps({"job_id": unique_id}), 200)
 
 
 @app.post("/inpaint")
-async def inpaint(request: Request):
+async def inpaint(request: Request, background_tasks: BackgroundTasks):
     headers = request.headers
 
     api_token_ = headers.get("api-token")
@@ -154,12 +178,20 @@ async def inpaint(request: Request):
     else:
         h = 512
 
-    image = SDApi.inpaint(prompt, init_image, mask_image, h, w)
-    return Response(json.dumps({"image": image}), 200)
+    os.makedirs(output_files_path + api_token_ + "/", exist_ok=True)
+    
+    unique_id = str(uuid.uuid4())
+    file_path =  output_files_path + api_token_ + "/" + unique_id + ".png"
+
+    background_tasks.add_task(run_task, SDApi.inpaint, prompt, init_image, mask_image, h, w, file_path, background_tasks)
+
+
+    return Response(json.dumps({"job_id": unique_id}), 200)
+
 
 
 @app.post("/imagemixer")
-async def image_mixer(request: Request):
+async def image_mixer(request: Request, background_tasks: BackgroundTasks):
 
     headers = request.headers
 
@@ -194,12 +226,19 @@ async def image_mixer(request: Request):
     else:
         h = 512
 
-    image = SDApi.image_mixer(init_image, style_image, h, w)
-    return Response(json.dumps({"image": image}), 200)
+    os.makedirs(output_files_path + api_token_ + "/", exist_ok=True)
+    
+    unique_id = str(uuid.uuid4())
+    file_path =  output_files_path + api_token_ + "/" + unique_id + ".png"
+
+    background_tasks.add_task(run_task, SDApi.image_mixer, init_image, style_image, h, w, file_path, background_tasks)
+
+
+    return Response(json.dumps({"job_id": unique_id}), 200)
 
 
 @app.post("/rembg")
-async def rembg(request: Request):
+async def rembg(request: Request, background_tasks: BackgroundTasks):
     headers = request.headers
 
     api_token_ = headers.get("api-token")
@@ -219,13 +258,17 @@ async def rembg(request: Request):
 
     image = body['image']
 
-
-    image = SDApi.remove_background(image)
-    return Response(json.dumps({"image": image}), 200)
+    os.makedirs(output_files_path + api_token_ + "/", exist_ok=True)
     
+    unique_id = str(uuid.uuid4())
+    file_path =  output_files_path + api_token_ + "/" + unique_id + ".png"
+
+    background_tasks.add_task(run_task, SDApi.remove_background, image, file_path, background_tasks)
+
+    return Response(json.dumps({"job_id": unique_id}), 200)
 
 @app.post("/magicmix")
-async def magic_mix(request: Request):
+async def magic_mix(request: Request, background_tasks: BackgroundTasks):
     headers = request.headers
 
     api_token_ = headers.get("api-token")
@@ -258,12 +301,18 @@ async def magic_mix(request: Request):
     else:
         h = 512
 
-    image = SDApi.magic_mix(image, prompt, h, w)
-    return Response(json.dumps({"image": image}), 200)
+    os.makedirs(output_files_path + api_token_ + "/", exist_ok=True)
+    
+    unique_id = str(uuid.uuid4())
+    file_path =  output_files_path + api_token_ + "/" + unique_id + ".png"
+
+    background_tasks.add_task(run_task, SDApi.magic_mix, image, prompt, h, w, file_path, background_tasks)
+
+    return Response(json.dumps({"job_id": unique_id}), 200)
 
 
 @app.post("/outpainting")
-async def outpainting(request: Request):
+async def outpainting(request: Request, background_tasks: BackgroundTasks):
     headers = request.headers
 
     api_token_ = headers.get("api-token")
@@ -294,12 +343,19 @@ async def outpainting(request: Request):
     image = body['image']
 
 
-    image = SDApi.outpainting(image, direction, prompt)
-    return Response(json.dumps({"image": image}), 200)
+    os.makedirs(output_files_path + api_token_ + "/", exist_ok=True)
+    
+    unique_id = str(uuid.uuid4())
+    file_path =  output_files_path + api_token_ + "/" + unique_id + ".png"
+
+    background_tasks.add_task(run_task, SDApi.outpainting, image, direction, prompt, file_path, background_tasks)
+
+    return Response(json.dumps({"job_id": unique_id}), 200)
+
 
 
 @app.post("/facegen")
-async def face_gen(request: Request):
+async def face_gen(request: Request, background_tasks: BackgroundTasks):
     headers = request.headers
 
     api_token_ = headers.get("api-token")
@@ -332,12 +388,18 @@ async def face_gen(request: Request):
     else:
         h = 512
 
-    image = SDApi.face_gen(image, prompt, h, w)
-    return Response(json.dumps({"image": image}), 200)
+    os.makedirs(output_files_path + api_token_ + "/", exist_ok=True)
+    
+    unique_id = str(uuid.uuid4())
+    file_path =  output_files_path + api_token_ + "/" + unique_id + ".png"
+
+    background_tasks.add_task(run_task, SDApi.face_gen, image, prompt, h, w, file_path, background_tasks)
+
+    return Response(json.dumps({"job_id": unique_id}), 200)
 
 
-@app.post("/upscale/set_task")
-async def set_task(request: Request):
+@app.post("/upscale")
+async def upscale(request: Request, background_tasks: BackgroundTasks):
     headers = request.headers
 
     api_token_ = headers.get("api-token")
@@ -351,19 +413,21 @@ async def set_task(request: Request):
         return Response(json.dumps({"message": "Could not decode body as JSON"}), 400)
     
 
-    if "file_id" not in body.keys():
-        return Response(json.dumps({"message": "No file id"}), 400)
+    if "image" not in body.keys():
+        return Response(json.dumps({"message": "There is no image in body"}), 400)
     
-    found_file = find_file(body["file_id"], files_dir_path + api_token_ + "/")
- 
-    if found_file is None:
-        return Response(json.dumps({"message": "No file found"}), 400)
+    os.makedirs(output_files_path + api_token_ + "/", exist_ok=True)
+    os.makedirs(files_dir_path + api_token_ + "/", exist_ok=True)
     
-    target_file = body["file_id"] + found_file
+    unique_id = str(uuid.uuid4())
+    file_path =  output_files_path + api_token_ + "/" + unique_id + ".png"
+    target_file =  files_dir_path + api_token_ + "/" + unique_id + ".png"
+    cv2.imwrite(target_file, ip.b64_cv_im(body['image']))
+
 
     subprocess_data = {
-        "-i" : files_dir_path + api_token_ + "/" + target_file,
-        "-o" : output_files_path + api_token_ + "/" + target_file,
+        "-i" : target_file,
+        "-o" : file_path,
         "-s" : "2" if "scale" not in body.keys() else body["scale"] if int(body["scale"]) <= 4 else "2",
         "-n" : "realesrgan-x4plus" if "model" not in body.keys() else body["model"] if body["model"] in models_list else "realesrgan-x4plus",
     }
@@ -381,45 +445,15 @@ async def set_task(request: Request):
 
     run_cmd(cmd)
 
-    return Response(json.dumps({"message": "Success"}), 200)
+    background_tasks.add_task(remove_file_delay, file_path, delay = del_delay)
+    background_tasks.add_task(remove_file_delay, target_file, delay = del_delay)
+
+    return Response(json.dumps({"job_id": unique_id}), 200)
 
 
-@app.post("/upscale/upload_file")
-async def upload_file(request: Request):
 
-    headers = dict(request.headers)
-
-    api_token_ = str(headers.get("api-token"))
-
-    if api_token_ not in api_tokens:
-        return Response(json.dumps({"message": "Invalid api token"}), 403)
-    
-    body_bytes = await request.body()
-    try:
-        body = dict(json.loads(body_bytes))
-    except json.JSONDecodeError:
-        return Response(json.dumps({"message": "Could not decode body as JSON"}), 400)
-    
-    if "image" not in body.keys():
-        return Response(json.dumps({"message": "There is no image in body"}), 400)
-
-    extension = ".png"
-    
-    unique_id = str(uuid.uuid4())
-    unique_filename = unique_id + extension
-
-    os.makedirs(files_dir_path + api_token_ + "/", exist_ok=True)
-    os.makedirs(output_files_path + api_token_ + "/", exist_ok=True)
-    file_location = files_dir_path + api_token_ + "/" +  unique_filename
-
-
-    cv2.imwrite(file_location, ip.b64_cv_im(body["image"]))
-
-    return Response(json.dumps({"id": unique_id}), 200)
-
-
-@app.post("/upscale/get_file")
-async def get_file(request: Request, background_tasks: BackgroundTasks):
+@app.post("/get_file")
+async def get_file(request: Request):
     headers = dict(request.headers)
 
 
@@ -433,26 +467,16 @@ async def get_file(request: Request, background_tasks: BackgroundTasks):
     except json.JSONDecodeError:
         return Response(json.dumps({"message": "Could not decode body as JSON"}), 400)
     
-    if "file_id" not in body.keys():
-        return Response(json.dumps({"message": "No file id"}), 400)
+    if "job_id" not in body.keys():
+        return Response(json.dumps({"message": "No job id"}), 400)
     
-    found_file = find_file(body["file_id"], files_dir_path)
+    if not os.path.isfile(output_files_path + api_token_ + "/" +  body["job_id"] + ".png"):
+        return Response(json.dumps({"message": "File processing doesn't done or save file time is expired, please wait or retry some method"}), 400)
 
-    if found_file is None:
-        return Response(json.dumps({"message": "No file found"}), 400)
-    
-    target_file = body["file_id"] + found_file
-    
-    file_path = output_files_path + api_token_ + "/" +  target_file
-    input_file_path = files_dir_path + api_token_ + "/" +  target_file
 
-    if not os.path.exists(file_path):
-        return Response(json.dumps({"message": "File processing doesn't done, please wait"}), 400)
-    
-    time.sleep(3)
+    file_path = output_files_path + api_token_ + "/" +  body["job_id"] + ".png"
 
-    background_tasks.add_task(remove_file_delay, file_path, delay = del_delay)
-    background_tasks.add_task(remove_file_delay, input_file_path, delay = del_delay)
+    time.sleep(2)
 
     return Response(json.dumps({"image": ip.cv_b64_im(cv2.imread(file_path))}), 200)
 
